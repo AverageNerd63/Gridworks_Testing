0e8c93753798fb2e4441761e460f09ccce2c219c
Ashton Smith <averagenerd00gaming@gmail.com>
2026-06-27 12:31:51 -0400

    feat(math): Add 3D math library

---
+++ src/math/gw_math.c
@@ -0,0 +1,163 @@
+#include "gw_math.h"
+
+/* ---- Mat4 ------------------------------------------------------------- */
+
+Mat4 mat4_identity(void) {
+    Mat4 m = {0};
+    m.m[0][0] = m.m[1][1] = m.m[2][2] = m.m[3][3] = 1.0f;
+    return m;
+}
+
+Mat4 mat4_mul(Mat4 a, Mat4 b) {
+    Mat4 r = {0};
+    for (int col = 0; col < 4; col++)
+        for (int row = 0; row < 4; row++)
+            for (int k = 0; k < 4; k++)
+                r.m[col][row] += a.m[k][row] * b.m[col][k];
+    return r;
+}
+
+Mat4 mat4_transpose(Mat4 m) {
+    Mat4 r;
+    for (int c = 0; c < 4; c++)
+        for (int r2 = 0; r2 < 4; r2++)
+            r.m[c][r2] = m.m[r2][c];
+    return r;
+}
+
+Mat4 mat4_translate(Vec3 t) {
+    Mat4 m = mat4_identity();
+    m.m[3][0] = t.x;
+    m.m[3][1] = t.y;
+    m.m[3][2] = t.z;
+    return m;
+}
+
+Mat4 mat4_scale_v(Vec3 s) {
+    Mat4 m = mat4_identity();
+    m.m[0][0] = s.x;
+    m.m[1][1] = s.y;
+    m.m[2][2] = s.z;
+    return m;
+}
+
+Mat4 mat4_rotate(Vec3 axis, f32 rad) {
+    Vec3  a = vec3_norm(axis);
+    f32   c = cosf(rad), s = sinf(rad), t = 1.0f - c;
+    Mat4  m = mat4_identity();
+    m.m[0][0] = t*a.x*a.x + c;
+    m.m[0][1] = t*a.x*a.y + s*a.z;
+    m.m[0][2] = t*a.x*a.z - s*a.y;
+    m.m[1][0] = t*a.x*a.y - s*a.z;
+    m.m[1][1] = t*a.y*a.y + c;
+    m.m[1][2] = t*a.y*a.z + s*a.x;
+    m.m[2][0] = t*a.x*a.z + s*a.y;
+    m.m[2][1] = t*a.y*a.z - s*a.x;
+    m.m[2][2] = t*a.z*a.z + c;
+    return m;
+}
+
+Mat4 mat4_perspective(f32 fov_y_rad, f32 aspect, f32 near, f32 far) {
+    f32  f = 1.0f / tanf(fov_y_rad * 0.5f);
+    Mat4 m = {0};
+    m.m[0][0] =  f / aspect;
+    m.m[1][1] =  f;
+    m.m[2][2] =  far / (near - far);           /* Vulkan depth [0,1] */
+    m.m[2][3] = -1.0f;
+    m.m[3][2] =  (near * far) / (near - far);
+    return m;
+}
+
+Mat4 mat4_look_at(Vec3 eye, Vec3 center, Vec3 up) {
+    Vec3 f = vec3_norm(vec3_sub(center, eye));
+    Vec3 r = vec3_norm(vec3_cross(f, up));
+    Vec3 u = vec3_cross(r, f);
+    Mat4 m = mat4_identity();
+    m.m[0][0] =  r.x; m.m[1][0] =  r.y; m.m[2][0] =  r.z;
+    m.m[0][1] =  u.x; m.m[1][1] =  u.y; m.m[2][1] =  u.z;
+    m.m[0][2] = -f.x; m.m[1][2] = -f.y; m.m[2][2] = -f.z;
+    m.m[3][0] = -vec3_dot(r, eye);
+    m.m[3][1] = -vec3_dot(u, eye);
+    m.m[3][2] =  vec3_dot(f, eye);
+    return m;
+}
+
+Vec4 mat4_mul_vec4(Mat4 m, Vec4 v) {
+    return (Vec4){{
+        m.m[0][0]*v.x + m.m[1][0]*v.y + m.m[2][0]*v.z + m.m[3][0]*v.w,
+        m.m[0][1]*v.x + m.m[1][1]*v.y + m.m[2][1]*v.z + m.m[3][1]*v.w,
+        m.m[0][2]*v.x + m.m[1][2]*v.y + m.m[2][2]*v.z + m.m[3][2]*v.w,
+        m.m[0][3]*v.x + m.m[1][3]*v.y + m.m[2][3]*v.z + m.m[3][3]*v.w,
+    }};
+}
+
+/* ---- Quat ------------------------------------------------------------- */
+
+Quat quat_identity(void) {
+    return (Quat){{0.0f, 0.0f, 0.0f, 1.0f}};
+}
+
+Quat quat_from_axis_angle(Vec3 axis, f32 rad) {
+    f32 s = sinf(rad * 0.5f);
+    return (Quat){{ axis.x*s, axis.y*s, axis.z*s, cosf(rad * 0.5f) }};
+}
+
+Quat quat_mul(Quat a, Quat b) {
+    return (Quat){{
+        a.w*b.x + a.x*b.w + a.y*b.z - a.z*b.y,
+        a.w*b.y - a.x*b.z + a.y*b.w + a.z*b.x,
+        a.w*b.z + a.x*b.y - a.y*b.x + a.z*b.w,
+        a.w*b.w - a.x*b.x - a.y*b.y - a.z*b.z,
+    }};
+}
+
+Quat quat_norm(Quat q) {
+    f32 l = sqrtf(q.x*q.x + q.y*q.y + q.z*q.z + q.w*q.w);
+    return l ? (Quat){{q.x/l, q.y/l, q.z/l, q.w/l}} : quat_identity();
+}
+
+Quat quat_slerp(Quat a, Quat b, f32 t) {
+    f32 dot = a.x*b.x + a.y*b.y + a.z*b.z + a.w*b.w;
+    if (dot < 0.0f) {
+        b = (Quat){{-b.x,-b.y,-b.z,-b.w}};
+        dot = -dot;
+    }
+    if (dot > 0.9995f) {
+        Quat r = {{
+            a.x + t*(b.x-a.x), a.y + t*(b.y-a.y),
+            a.z + t*(b.z-a.z), a.w + t*(b.w-a.w)
+        }};
+        return quat_norm(r);
+    }
+    f32 theta0 = acosf(dot);
+    f32 theta  = theta0 * t;
+    f32 sa = sinf(theta0), sb = sinf(theta0 - theta);
+    f32 sc = sinf(theta);
+    return (Quat){{
+        (a.x*sb + b.x*sc) / sa,
+        (a.y*sb + b.y*sc) / sa,
+        (a.z*sb + b.z*sc) / sa,
+        (a.w*sb + b.w*sc) / sa,
+    }};
+}
+
+Mat4 quat_to_mat4(Quat q) {
+    f32 xx=q.x*q.x, yy=q.y*q.y, zz=q.z*q.z;
+    f32 xy=q.x*q.y, xz=q.x*q.z, yz=q.y*q.z;
+    f32 wx=q.w*q.x, wy=q.w*q.y, wz=q.w*q.z;
+    Mat4 m = mat4_identity();
+    m.m[0][0]=1-2*(yy+zz); m.m[0][1]=2*(xy+wz);   m.m[0][2]=2*(xz-wy);
+    m.m[1][0]=2*(xy-wz);   m.m[1][1]=1-2*(xx+zz); m.m[1][2]=2*(yz+wx);
+    m.m[2][0]=2*(xz+wy);   m.m[2][1]=2*(yz-wx);   m.m[2][2]=1-2*(xx+yy);
+    return m;
+}
+
+Vec3 quat_rotate_vec3(Quat q, Vec3 v) {
+    Vec3 u  = {{q.x, q.y, q.z}};
+    f32  s  = q.w;
+    Vec3 r  = vec3_add(
+                vec3_add(vec3_scale(u, 2.0f * vec3_dot(u, v)),
+                         vec3_scale(v, s*s - vec3_dot(u, u))),
+                vec3_scale(vec3_cross(u, v), 2.0f * s));
+    return r;
+}
---
+++ src/math/gw_math.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <math.h>
+#include "../core/types.h"
+
+/* ---- Types ------------------------------------------------------------ */
+
+typedef union { struct { f32 x, y; };       f32 v[2]; } Vec2;
+typedef union { struct { f32 x, y, z; };    f32 v[3]; } Vec3;
+typedef union { struct { f32 x, y, z, w; }; f32 v[4]; } Vec4;
+typedef union { struct { f32 x, y, z, w; }; f32 v[4]; } Quat;
+
+/* Column-major, matches Vulkan/GLSL layout */
+typedef struct { f32 m[4][4]; } Mat4;
+
+/* ---- Constants -------------------------------------------------------- */
+
+#define GW_PI      3.14159265358979323846f
+#define GW_DEG2RAD (GW_PI / 180.0f)
+#define GW_RAD2DEG (180.0f / GW_PI)
+
+/* ---- Vec2 ------------------------------------------------------------- */
+
+static inline Vec2 vec2(f32 x, f32 y)            { return (Vec2){{x, y}}; }
+static inline Vec2 vec2_add(Vec2 a, Vec2 b)       { return (Vec2){{a.x+b.x, a.y+b.y}}; }
+static inline Vec2 vec2_sub(Vec2 a, Vec2 b)       { return (Vec2){{a.x-b.x, a.y-b.y}}; }
+static inline Vec2 vec2_scale(Vec2 a, f32 s)      { return (Vec2){{a.x*s, a.y*s}}; }
+static inline f32  vec2_dot(Vec2 a, Vec2 b)       { return a.x*b.x + a.y*b.y; }
+static inline f32  vec2_len(Vec2 a)               { return sqrtf(vec2_dot(a, a)); }
+static inline Vec2 vec2_norm(Vec2 a)              { f32 l = vec2_len(a); return l ? vec2_scale(a, 1.0f/l) : a; }
+
+/* ---- Vec3 ------------------------------------------------------------- */
+
+static inline Vec3 vec3(f32 x, f32 y, f32 z)     { return (Vec3){{x, y, z}}; }
+static inline Vec3 vec3_add(Vec3 a, Vec3 b)       { return (Vec3){{a.x+b.x, a.y+b.y, a.z+b.z}}; }
+static inline Vec3 vec3_sub(Vec3 a, Vec3 b)       { return (Vec3){{a.x-b.x, a.y-b.y, a.z-b.z}}; }
+static inline Vec3 vec3_scale(Vec3 a, f32 s)      { return (Vec3){{a.x*s, a.y*s, a.z*s}}; }
+static inline f32  vec3_dot(Vec3 a, Vec3 b)       { return a.x*b.x + a.y*b.y + a.z*b.z; }
+static inline f32  vec3_len(Vec3 a)               { return sqrtf(vec3_dot(a, a)); }
+static inline Vec3 vec3_norm(Vec3 a)              { f32 l = vec3_len(a); return l ? vec3_scale(a, 1.0f/l) : a; }
+static inline Vec3 vec3_lerp(Vec3 a, Vec3 b, f32 t) {
+    return vec3_add(vec3_scale(a, 1.0f - t), vec3_scale(b, t));
+}
+static inline Vec3 vec3_cross(Vec3 a, Vec3 b) {
+    return (Vec3){{
+        a.y*b.z - a.z*b.y,
+        a.z*b.x - a.x*b.z,
+        a.x*b.y - a.y*b.x
+    }};
+}
+
+/* ---- Vec4 ------------------------------------------------------------- */
+
+static inline Vec4 vec4(f32 x, f32 y, f32 z, f32 w) { return (Vec4){{x, y, z, w}}; }
+static inline Vec4 vec4_add(Vec4 a, Vec4 b)  { return (Vec4){{a.x+b.x, a.y+b.y, a.z+b.z, a.w+b.w}}; }
+static inline Vec4 vec4_scale(Vec4 a, f32 s) { return (Vec4){{a.x*s, a.y*s, a.z*s, a.w*s}}; }
+
+/* ---- Mat4 ------------------------------------------------------------- */
+
+Mat4 mat4_identity(void);
+Mat4 mat4_mul(Mat4 a, Mat4 b);
+Mat4 mat4_transpose(Mat4 m);
+Mat4 mat4_translate(Vec3 t);
+Mat4 mat4_scale_v(Vec3 s);
+Mat4 mat4_rotate(Vec3 axis, f32 radians);
+Mat4 mat4_perspective(f32 fov_y_rad, f32 aspect, f32 near, f32 far);
+Mat4 mat4_look_at(Vec3 eye, Vec3 center, Vec3 up);
+Vec4 mat4_mul_vec4(Mat4 m, Vec4 v);
+
+/* ---- Quat ------------------------------------------------------------- */
+
+Quat quat_identity(void);
+Quat quat_from_axis_angle(Vec3 axis, f32 radians);
+Quat quat_mul(Quat a, Quat b);
+Quat quat_norm(Quat q);
+Quat quat_slerp(Quat a, Quat b, f32 t);
+Mat4 quat_to_mat4(Quat q);
+Vec3 quat_rotate_vec3(Quat q, Vec3 v);
